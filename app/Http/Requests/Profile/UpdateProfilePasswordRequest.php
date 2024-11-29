3d27f3e608cedab4fcfb7c03891ab6e04c8be1d4
gog-2210 <guynhuynh123@gmail.com>
2024-11-30 05:00:36 +0700

    updated structure

--- app/Http/Requests/Profile/UpdateProfilePasswordRequest.php
+++ app/Http/Requests/Profile/UpdateProfilePasswordRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Frontend;
+namespace App\Http\Requests\Profile;
 
 use Illuminate\Foundation\Http\FormRequest;
 
--- app/Http/Requests/Profile/UpdateProfileRequest.php
+++ app/Http/Requests/Profile/UpdateProfileRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Frontend;
+namespace App\Http\Requests\Profile;
 
 use Auth;
 use Illuminate\Foundation\Http\FormRequest;
